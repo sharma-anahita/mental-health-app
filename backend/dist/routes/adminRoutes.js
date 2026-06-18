d048a71f2a3f8aeab33bffb492d89929b538c419
Anahita Sharma <sharma.anahita.as@gmail.com>
2026-06-18 20:30:31 +0530

    chore: remove build artifacts from repository

--- backend/dist/routes/adminRoutes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const adminController_1 = require("../controllers/adminController");
-const router = (0, express_1.Router)();
-// POST /api/admin/seed-store
-// Requires x-admin-secret header
-router.post('/seed-store', adminController_1.seedStoreData);
-exports.default = router;
